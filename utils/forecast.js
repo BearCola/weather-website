2e7053211982ae842acc5dac6d7f824cfa0e9059
Sangin <sangin.kang@navercorp.com>
2019-03-19 08:53:02 +0900

    Init commit

---
+++ utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (lat, lon, callback) => {
+
+    const url = `https://api.darksky.net/forecast/1fdf11b341f856160460f70630c62629/${lat},${lon}`
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+    
+}
+
+
+module.exports = forecast
